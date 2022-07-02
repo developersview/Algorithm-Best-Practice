ddfaf4dccb2fd95af875af07c0dc544f65590ff9
developersview <pcslg1998@gmail.com>
2022-07-02 22:06:27 +0530

    updated LinearSearch.js

---
+++ Linear Search/LinearSearch.java
@@ -0,0 +1,10 @@
+public class LinearSearch {
+    public int linearSearch(int[] arr, int x){
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]==x)
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {5, 8, 6, 1, 9, 10};
+    }
+}
---
+++ Linear Search/LinearSearch.js
@@ -0,0 +1,14 @@
+function linearSearch(arr, x) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === x) return i;
+    }
+    return -1;
+}
+let array = [5, 8, 6, 1, 9, 10];
+let key = 1;
+let result = linearSearch(array, key);
+if (result != -1) {
+    console.log(`The number ${key} is in the position of ${result}`);
+} else {
+    console.log(`The number ${key} is not present in the array`);
+}
