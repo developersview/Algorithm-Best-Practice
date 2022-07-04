5d41fbae6b22a2af705626099b3894958f3852b0
developersview <pcslg1998@gmail.com>
2022-07-04 13:58:56 +0530

    updated Insertion Sort with java and js

---
+++ 4_Insertion_Sort/InsertionSort.java
@@ -0,0 +1,25 @@
+//insertion sort algorithm implementation
+public class InsertionSort {
+    public static int[] insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; //element at 1 postion for 1st time run
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+
+    //driver code
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+        int[] result = insertionSort(array);
+        for (int element : result) {
+            System.out.printf(element + " ");
+        }
+    }
+}
---
+++ 4_Insertion_Sort/InsertionSort.js
@@ -0,0 +1,17 @@
+function insertionSort(arr) {
+    let n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+//driver code
+let array = [12, 11, 13, 5, 6];
+console.log(insertionSort(array));
