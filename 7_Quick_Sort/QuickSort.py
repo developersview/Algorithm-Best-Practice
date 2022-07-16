005ebb8077e3a0b76eaed74fabc1fd54c463d4bc
developersview <pcslg1998@gmail.com>
2022-07-16 14:09:19 +0530

    corrected quicksort.py code

--- 7_Quick_Sort/QuickSort.py
+++ 7_Quick_Sort/QuickSort.py
@@ -1,16 +1,16 @@
 def partition(arr,low,high):
     pivot=array[high]
     i = low-1
-    for j in range(low,high-1):
-        if(arr[j] < pivot):
-            i += 1
+    for j in range(low,high):
+        if(arr[j] <= pivot):
+            i = i+ 1
             arr[i], arr[j] = arr[j], arr[i]
     
     arr[i+1], arr[high] = arr[high], arr[i+1]
     return i+1
 
 def quickSort(arr, low, high):
-    if(low<high):
+    if(low < high):
         pivot_index = partition(arr, low, high)
         quickSort(arr, low, pivot_index - 1)
         quickSort(arr, pivot_index + 1, high)
